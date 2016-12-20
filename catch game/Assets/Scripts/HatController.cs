03084271a6ebaed47edab1684948eb372e0a7a19
Kevin <kevin.gross@tk-squared.com>
2016-12-20 14:15:27 -0500

    webgl build with mouse controls for the hat, so cool so hip so hat

---
+++ catch game/Assets/Scripts/HatController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatController : MonoBehaviour {
+
+    public Camera cam;
+	// Use this for initialization
+	void Start () {
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+	}
+	
+	// Update is called once per PHYSICS TIMESTEP
+	void FixedUpdate () {
+		Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 targetPosition = new Vector3(rawPosition.x, 0.0f, 0.0f);
+        Rigidbody2D body = (Rigidbody2D)this.GetComponent("Rigidbody2D");
+        body.MovePosition(targetPosition);
+    }
+}
